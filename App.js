f53ddb4e007879b3be368bb484761b35d2529fe0
Steve Rhoads <srhoads@rallydev.com>
2014-10-30 23:58:28 +0000

    Added Iteration and Release Combos with Layout:HBox

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'My Second ZZyzxApp!'},
+    items:[
+        { html:'My Second ZZyzxApp!'},
+        {xtype: 'rallyiterationcombobox'},
+        {xtype: 'rallyreleasecombobox'}
+    ],
+    layout: {type: 'hbox'},
     launch: function() {
         //Write app code here
-        console.log("Our Second App has Arrived")
+        console.log("Our Second App has Arrived");
     }
 });
